d64cf3f1a19f8547adaab19f5fcd19fe628ef40d
mf470 <56916618+mf470@users.noreply.github.com>
2021-07-16 16:37:32 +0100

    Make shift private Implement * using shift

--- AES/Main.cpp
+++ AES/Main.cpp
@@ -2,9 +2,12 @@
 
 
 int main() {
-	Rijndael a(13);
+	Rijndael a(27);
 	cout << +(a.coefs.ch) << endl;
-	Rijndael b = *(a.inv());
-	cout << +(b.coefs.ch) << endl;
+	unsigned char deg;
+	for (deg = 0; a.coefs.ch >> deg != 0; deg++);
+	cout << +deg << endl;
+	/*Rijndael b = *(a.inv());
+	cout << +(b.coefs.ch) << endl;*/
 	cin.get();
 }
