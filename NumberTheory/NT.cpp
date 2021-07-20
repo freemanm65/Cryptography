d0c9dec36aec73c3815513938095172c84804f30
freemanm65 <44925374+freemanm65@users.noreply.github.com>
2021-07-20 16:55:24 +0100

    expmod and sqrtmodp

--- NumberTheory/NT.cpp
+++ NumberTheory/NT.cpp
@@ -47,3 +47,48 @@ char jacobi(int a, int n)
 
 	return result * jacobi(n % a, a);
 }
+
+int sqrtmodp(int a, int p)
+{
+	a %= p;
+	if (p % 8 == 3 || p % 8 == 7) 
+		return expmod(a, (p + 1) >> 2, p);
+	if (p % 8 == 5) {
+		int x = expmod(a, (p + 3) >> 3, p);
+		int c = (x * x) % p;
+		if (c != a)
+			x = x * expmod(2, (p + 1) >> 2, p);
+		return x;
+	}
+	srand(time(0));
+	int d = 2;
+	while (jacobi(d, p) != 1)
+		d = rand() % (p - 2) + 2;
+	int k = 0;
+	int q = p-1;
+	while (!(q % 2)) {
+		q >>= 1;
+		k++;
+	}
+	int A = expmod(a, q, p);
+	int D = expmod(d, q, p);
+	int m = 0;
+	for (int i = 0; i < k; i++) {
+		if (expmod(A * expmod(D, m, p), pow(2, k - 1 - i), p) == p - 1)
+			m += pow(2, i);
+	}
+	return expmod(a, (q + 1) >> 1, p) * expmod(D, m >> 1, p) % p;
+}
+
+int expmod(int b, int e, int m)
+{
+	int r = 1;
+	while (e > 0) {
+		if (e % 2)
+			r = (r * b) % m;
+		b = (b * b) % m;
+		e >>= 1;
+	}
+
+	return r;
+}
--- NumberTheory/NT.h
+++ NumberTheory/NT.h
@@ -1,4 +1,10 @@
 #pragma once
 
+#include <ctime>
+#include <stdlib.h>
+#include <cmath>
+
 int* eea(int, int);
 char jacobi(int, int);
+int sqrtmodp(int, int);
+int expmod(int, int, int);
