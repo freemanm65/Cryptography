e062348b9041ba9fb6c70b2485eaa937fefae701
freemanm65 <44925374+freemanm65@users.noreply.github.com>
2021-07-20 15:42:03 +0100

    Jacobi symbol implementation

--- NumberTheory/NT.cpp
+++ NumberTheory/NT.cpp
@@ -21,3 +21,29 @@ int* eea(int x, int y) {
 
 	return ret;
 }
+
+char jacobi(int a, int n)
+{
+	if (!(a % n))
+		return 0;
+
+	char result = 1;
+
+	if (a < 0) {
+		a = -a;
+		result *= n % 4 == 3 ? -1 : 1;
+	}
+
+	while (!(a % 2)) {
+		a >>= 1;
+		result *= n % 8 == 3 || n % 8 == 5 ? -1 : 1;
+	}
+
+	if (a == 1)
+		return result;
+
+	if (a % 4 == 3 && n % 4 == 3)
+		return -1 * result * jacobi(n % a, a);
+
+	return result * jacobi(n % a, a);
+}
--- NumberTheory/NT.h
+++ NumberTheory/NT.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int* eea(int, int);
+char jacobi(int, int);
